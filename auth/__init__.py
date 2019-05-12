508f88aa2760f1a5e5385828c12106ff65679271
LionTao <taojiachun980831@163.com>
2019-05-12 19:07:18 +0800

    roll back

--- auth/__init__.py
+++ auth/__init__.py
@@ -3,78 +3,65 @@ from auth.login import dologin
 from flask_cors import CORS
 import hashlib
 import os
-import logging
 
 from auth.sso import checkMyauth
 
-if "SECRET" in os.environ:
-    se = os.environ.get("SECRET")
-    logging.info("SECRET SET")
-else:
-    se = "changethis"
-
 SECRET = hashlib.md5()
-SECRET.update(se.encode('utf8'))
+SECRET.update(os.environ.get('SECRET').encode('utf8'))
 SECRET = SECRET.hexdigest()
 keys = {'id_tag', 'secret', 'timestamp', 'name', 'usertype'}
 
 if os.path.exists('/run/secrets/sso-passwd'):
     with open('/run/secrets/sso-passwd', 'r') as fp:
         passwd = fp.readline()
-        logging.info("SSO_PASSWD get from secrets file")
 elif 'passwd' in os.environ.keys():
     passwd = os.environ.get('SSO_PASSWD')
-    logging.info("SSO_PASSWD SET")
 else:
-    passwd = 'changeit'
+    passwd = 'changethis'
     print("SSO Passwd not Found.\n Service may not work properly")
 
-# create and configure the app
-app = Flask(__name__)
-CORS(app=app)
 
+def create_app():
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app=app, supports_credentials=True)
 
-# a simple page that says hello
-@app.route('/healthcheck')
-def healthcheck():
-    return "I'm ok", 200
+    # a simple page that says hello
+    @app.route('/healthcheck/')
+    def healthcheck():
+        return "I'm ok"
 
+    @app.route('/logout')
+    def logouter():
+        return 'Logouted'
 
-@app.route('/logout')
-def logouter():
-    return 'Logouted', 200
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # for clients
-        data = request.json
-        if data.get('id') and data.get('token'):
-            status, msg = dologin(data, secret=SECRET)
-            return jsonify({"ok": status, "message": msg}), 200
-        else:
-            return jsonify({"ok": False, "message": "wrong params"}), 400
-
-    elif request.method == 'GET':
-        # for ids service of Soochow university
-        params = set([i for i in request.args.keys()])
-        # print(params)
-        if keys.issubset(params):
-            # print(request.args)
-            # print(type(keys))
-            id_tag = request.args['id_tag']
-            secret = request.args['secret']
-            timestamp = request.args['timestamp']
-            name = request.args['name']
-            usertype = request.args['usertype']
-            print("id_tag:{}\tsecret:{}\ttimestamp:{}".format(id_tag, secret, timestamp))
-            logging.info(("id_tag:{}\tsecret:{}\ttimestamp:{}".format(id_tag, secret, timestamp)))
-            res = checkMyauth(id_tag=id_tag, secret=secret, clienttime=timestamp, passwd=passwd)
-            #print("passwd: ",passwd)
-            if res:
-                return jsonify({"status": True, "data": {"id": id_tag, "name": name, "usertype": usertype}}), 200
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            data = request.args.to_dict()
+            if data.get('id') and data.get('token'):
+                status, msg = dologin(data, secret=SECRET)
+                return jsonify({"ok": status, "message": msg}), 200
             else:
-                return jsonify({"error": "Time out", "data": "Secret check failed."}), 200
-        else:
-            return jsonify({"error": "Lacking parameters", "data": "Please ask admin"}), 400
+                return jsonify({"ok": False, "message": "wrong params"}), 400
+        elif request.method == 'GET':
+            params = set([i for i in request.args.keys()])
+            # print(params)
+            if keys.issubset(params):
+                # print(request.args)
+                # print(type(keys))
+                id_tag = request.args['id_tag']
+                secret = request.args['secret']
+                timestamp = request.args['timestamp']
+                name = request.args['name']
+                usertype = request.args['usertype']
+                print("id_tag:{}\tsecret:{}\ttimestamp:{}".format(id_tag, secret, timestamp))
+                res = checkMyauth(id_tag=id_tag, secret=secret, clienttime=timestamp, passwd=passwd)
+                if res:
+                    return jsonify({"status": "ok", "data": {"id": id_tag, "name": name, "usertype": usertype}})
+                else:
+                    return jsonify({"status": "error", "data": "Timed Out"})
+            else:
+                return jsonify({"status": "error", "data": "Lacking Params"})
+
+    return app
--- auth/sso.py
+++ auth/sso.py
@@ -7,10 +7,8 @@ def checkMyauth(id_tag: str, secret: str, clienttime: str, passwd: str) -> bool:
     if int(systemtime) - int(clienttime) > 60 * 60:
         return False
     secret1 = hashlib.md5()
-    secret1.update((str(id_tag) + str(passwd) + str(clienttime)).encode('utf8'))
+    secret1.update((id_tag + passwd + clienttime).encode('utf8'))
     print("mysecret:{}".format(secret1.hexdigest()))
-    print("secret1:", secret1.hexdigest(), " secret2: ", secret)
     if secret1.hexdigest() == secret:
         return True
-    # somehow md5 is broken
-    return True
+    return False
