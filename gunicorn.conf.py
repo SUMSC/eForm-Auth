32ecc67aef302c1ec3f0eeafad8354450b06f1c1
Amber <wzhzzmzzy@gmail.com>
2020-02-18 20:13:20 +0800

    clean gunicon conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,15 +1,10 @@
-import multiprocessing
-
-bind = '127.0.0.1:8001'
+bind = '127.0.0.1:8001'     # bind host:port
 workers = 2
 timeout = 10
 keepalive = 3
 daemon = True
-
-
 user = 'amber'
-
-worker_class = 'gevent'
+worker_class = 'gevent'     # run in gevent mode
 proc_name = 'gunicorn.proc'
 pidfile = '/home/amber/eForm-Backend/logs/eForm-Auth-0.pid'
 #loglevel = 'info'
